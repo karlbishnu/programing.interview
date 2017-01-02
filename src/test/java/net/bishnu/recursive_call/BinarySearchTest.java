f1e6b4c0fb9591828f7c488319832e7143f9db4e
Bishnu <karlbishnu@gmail.com>
2017-01-02 20:50:05 +0900

    recursive_call more precisely test

--- src/test/java/net/bishnu/recursive_call/BinarySearchTest.java
+++ src/test/java/net/bishnu/recursive_call/BinarySearchTest.java
@@ -9,31 +9,30 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 
 import static java.math.BigDecimal.ROUND_FLOOR;
-import static org.junit.Assert.*;
 
 /**
  * Created by karlb on 2017-01-02.
  */
 public class BinarySearchTest {
-    private final int[] array = new int[1000000];
-    private final int linearComplexity = array.length;
-    private final int logComplexity = new BigDecimal(Math.log(array.length)).setScale(0, ROUND_FLOOR).intValue();
+    private final int[] evenNumers = new int[1000000];
+    private final int linearComplexity = evenNumers.length;
+    private final int logComplexity = new BigDecimal(Math.log(evenNumers.length)).setScale(0, ROUND_FLOOR).intValue();
     @Before
     public void setUp() throws IOException {
-        Arrays.setAll(array, (x)->(x));
+        Arrays.setAll(evenNumers, (x)->(x*2));
     }
 
     @Test
     public void binSearch() throws Exception {
-        int index = BinarySearch.binSearch(array, array.length-1);
-        Assert.assertEquals(array.length-1, index);
+        int index = BinarySearch.binSearch(evenNumers, evenNumers[evenNumers.length-1]);
+        Assert.assertEquals(evenNumers.length-1, index);
         System.out.println("logComplexity="+logComplexity);
         System.out.println("call="+BinarySearch.call);
     }
 
     @Test
     public void binSearchNotFound() throws Exception {
-        int index = BinarySearch.binSearch(array, array.length);
+        int index = BinarySearch.binSearch(evenNumers, 13);
         Assert.assertEquals(-1, index);
         System.out.println("logComplexity="+logComplexity);
         System.out.println("call="+BinarySearch.call);
