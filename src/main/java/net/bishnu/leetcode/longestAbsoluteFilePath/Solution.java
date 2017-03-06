109622df09da5ee90eb3de49a153222878d9c238
mhryoo <mhryoo@konadev.com>
2017-03-06 15:20:18 +0900

    update 388. Longest Absolute Path

--- src/main/java/net/bishnu/leetcode/longestAbsoluteFilePath/Solution.java
+++ src/main/java/net/bishnu/leetcode/longestAbsoluteFilePath/Solution.java
@@ -1,134 +1,25 @@
 package net.bishnu.leetcode.longestAbsoluteFilePath;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by karlb on 2017-03-05.
  */
 public class Solution {
-    class Entity{
-        Map<String, Entity> map;
-        Entity parent;
-        int level;
-        String name;
-        boolean isDir;
-
-        Entity(Entity parent, String name, int level, boolean isDir){
-            this.parent = parent;
-            this.name = name;
-            this.level = level;
-            this.isDir = isDir;
-            if(isDir)   map = new HashMap<>();
-        }
-    }
-
-    private Entity root;
-
-    private int makeTree(String input, int offset, Entity dir){
-        String name = "";
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sbWithoutWhiteSpace = new StringBuilder();
-        int level = 0;
-        boolean isDir = true;
-
-        for(int i=offset; i<input.length(); i++){
-            char aChar = input.charAt(i);
-
-            if(aChar == '\n'){
-                name = isDir ? sbWithoutWhiteSpace.toString() : sb.toString();
-
-                Entity entity = new Entity(root, name, level, isDir);
-                if(isDir){
-                    i=makeTree(input, i+1, entity);
-                }
-
-                sb.setLength(0);
-                sbWithoutWhiteSpace.setLength(0);
-                level = 0;
-                isDir = true;
-            }else if(aChar =='/'){
-                Entity parent = getParent(dir, level);
-                name = isDir ? sbWithoutWhiteSpace.toString() : sb.toString();
-
-                Entity entity = new Entity(parent, name, level, isDir);
-                if(parent != null){
-                    parent.map.put(entity.name, entity);
-                }else {
-                    root = entity;
-                }
-
-                if(isDir){
-                    i=makeTree(input, i+1, entity);
-                }
-
-                sb.setLength(0);
-                sbWithoutWhiteSpace.setLength(0);
-                level = 0;
-                isDir = true;
-            }else if(aChar == '\t'){
-                level++;
-            }else {
-                if(aChar == '.'){
-                    isDir = false;
-                }
-
-                if(aChar == ' '){
-                    sb.append(aChar);
-                }else{
-                    sb.append(aChar);
-                    sbWithoutWhiteSpace.append(aChar);
-                }
-            }
-        }
-
-        if(sb.length()>0){
-            name = isDir ? sbWithoutWhiteSpace.toString() : sb.toString();
-            Entity parent = getParent(dir, level);
-            Entity entity = new Entity(parent, name, level, isDir);
-            if(parent != null){
-                parent.map.put(entity.name, entity);
-            }
-        }
-
-        return input.length();
-    }
-
-    private Entity getParent(Entity dir, int level) {
-        Entity curr = dir;
-        while(curr != null && level != -1 && curr.level >= level){
-            curr = curr.parent;
-        }
-        return curr;
-    }
-
-    private String traverse(Entity root){
-        String path = "";
-        if(root.isDir) {
-            Map<String, Entity> map = root.map;
-            Iterator<String> iterator = map.keySet().iterator();
-
-            while (iterator.hasNext()) {
-                Entity entity = map.get(iterator.next());
-                String aString = traverse(entity);
-                path = path.length() > aString.length()?
-                        path :
-                        aString;
-            }
-            path = root.name!=null ? root.name +"/"+path : path;
-        }else{
-            path = root.name;
-        }
-
-        return path;
-    }
-
     public int lengthLongestPath(String input){
-        input.replaceAll("\n\t", "/");
-        root = new Entity(null, null, -1, true);
-        makeTree(input, 0, root);
-        String longestPath = traverse(root);
-        return longestPath.contains(".") ? longestPath.length() : 0;
+        String[] entities = input.split("\n");
+        int maxPathLength = 0;
+        Deque<Integer> stack = new LinkedList<>();
+
+        stack.push(0);
+        for(String entity: entities){
+            int level = entity.lastIndexOf('\t')+1;
+            while(level<stack.size()-1)   stack.pop();
+            stack.push(stack.peek()+entity.length()-level+1);
+            if(entity.contains(".")){
+                maxPathLength = Math.max(maxPathLength, stack.peek()-1);
+            }
+        }
+        return maxPathLength;
     }
 }
