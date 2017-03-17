bd83ed2eab37f5f8523832d598605a0ed0e7f330
Bishnu <karlbishnu@gmail.com>
2017-03-17 21:28:36 +0900

    Add Code jam Main

---
+++ codejam/src/main/java/net/bishnu/codejam/Main.java
@@ -0,0 +1,65 @@
+package net.bishnu.codejam;
+
+/**
+ * Created by karlb on 2017-03-17.
+ */
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static final String NAME = "test";
+    private static boolean SUBMIT = false;
+    private static InputReader in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) throws IOException {
+        if (SUBMIT) {
+            in = new InputReader(new FileInputStream(new File(NAME + ".in")));
+            out = new PrintWriter(new BufferedWriter(new FileWriter(NAME + ".out")));
+        } else {
+            in = new InputReader(System.in);
+            out = new PrintWriter(System.out, true);
+        }
+
+        int numCases = in.nextInt();
+        for (int test = 1; test <= numCases; test++) {
+            out.print("Case #" + test + ": ");
+            main2();
+        }
+
+        out.close();
+        System.exit(0);
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    private static void main2() {
+
+    }
+
+}
