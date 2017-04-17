578bb009986c765f4bb8d7ac05b10775b28a583b
Bishnu <karlbishnu@gmail.com>
2017-04-17 22:33:21 +0900

    add 163. Missing Ranges

---
+++ src/main/java/net/bishnu/leetcode/missingRange/MissingRange.java
@@ -0,0 +1,14 @@
+package net.bishnu.leetcode.missingRange;
+
+import java.util.List;
+
+/**
+ * 163. Missing Ranges
+ * https://leetcode.com/problems/missing-ranges/#/description
+ * Created by karlb on 2017-04-17.
+ */
+public class MissingRange {
+    public static List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        return null;
+    }
+}
---
+++ src/test/java/net/bishnu/leetcode/missingRange/MissingRangeTest.java
@@ -0,0 +1,63 @@
+package net.bishnu.leetcode.missingRange;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by karlb on 2017-04-17.
+ */
+public class MissingRangeTest {
+    @Test
+    public void findMissingRanges() throws Exception {
+    }
+
+    @Test
+    public void findMissingRangesShouldReturnSzie0List() throws Exception {
+        assertEquals(0, MissingRange.findMissingRanges(new int[]{0,2,10}, 2, 2).size());
+    }
+
+    @Test
+    public void findMissingRangesShouldReturnSzie1List() throws Exception {
+        List<String> list = MissingRange.findMissingRanges(new int[]{0,10}, 2, 2);
+        assertEquals(1, list.size());
+        assertEquals("2", list.get(0));
+    }
+
+    @Test
+    public void findMissingRanges2() throws Exception {
+        int lower = 2;
+        int upper = 4;
+        List<String> list = MissingRange.findMissingRanges(new int[]{0,10}, lower, upper);
+        assertEquals(1, list.size());
+        assertEquals(lower+"->"+upper, list.get(0));
+    }
+
+    @Test
+    public void findMissingRanges3() throws Exception {
+        int lower = -1;
+        int upper = 11;
+        List<String> list = MissingRange.findMissingRanges(new int[]{0,10}, lower, upper);
+        List<String> expected = new LinkedList<>();
+        expected.add(String.valueOf(lower));
+        expected.add(String.valueOf(upper));
+        assertEquals(expected, list);
+    }
+
+    @Test
+    public void test(){
+        int[] array = new int[]{0, 1, 3, 50, 75};
+
+        assertEquals(0, Arrays.binarySearch(array, 0));
+        assertEquals(-1, Arrays.binarySearch(array, array[0]-1));
+        assertEquals(-(array.length+1), Arrays.binarySearch(array, array[array.length-1]+1));
+        assertEquals(array.length-1, Arrays.binarySearch(array, array[array.length-1]));
+        assertEquals(-4, Arrays.binarySearch(array, 4));
+        assertEquals(-3, Arrays.binarySearch(array, 2));
+    }
+
+}
