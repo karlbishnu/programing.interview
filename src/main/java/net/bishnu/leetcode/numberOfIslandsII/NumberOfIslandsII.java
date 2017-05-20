d202f362a0366d3e4f9121fd17387ce84cc12423
Bishnu <karlbishnu@gmail.com>
2017-05-20 15:03:21 +0900

    Update 305. Number of Islands II https://leetcode.com/problems/number-of-islands-ii/#/description
    timeout version

--- src/main/java/net/bishnu/leetcode/numberOfIslandsII/NumberOfIslandsII.java
+++ src/main/java/net/bishnu/leetcode/numberOfIslandsII/NumberOfIslandsII.java
@@ -1,9 +1,6 @@
 package net.bishnu.leetcode.numberOfIslandsII;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 305. Number of Islands II
@@ -11,50 +8,52 @@ import java.util.List;
  * Created by karlb on 2017-05-02.
  */
 public class NumberOfIslandsII {
-    private Integer[][] newMap(int m, int n){
-        Integer[][] map = new Integer[m][n];
-        for(Integer[] array: map)
-            Arrays.fill(array, 0);
-        return map;
-    }
 
+    private int[][] directions = new int[][]{{1,0}, {-1,0}, {0,0}, {0,1}, {0,-1}};
     public List<Integer> numIslands2(int m, int n, int[][] positions){
-        List<Integer[][]> maps = new LinkedList<>();
-        maps.add(newMap(m,n));
+        int[][] grid = new int[m][n];
         List<Integer> result = new ArrayList<>(positions.length);
-        for(int[] position: positions){
-            int placedCount = placeIsland(position, maps);
-            int after = maps.size();
-            result.add(after-placedCount);
+        for(int i=0; i<positions.length; i++){
+            int row = positions[i][0];
+            int col = positions[i][1];
+            result.add(addLand(grid, row, col));
         }
         return result;
     }
 
-    private int placeIsland(int[] position, List<Integer[][]> maps) {
-        int count = 0;
-        for(Integer[][] map: maps){
-            if(adjacentLandExist(map,position)){
-                map[position[0]][position[1]] = 1;
-                count++;
+    private Integer addLand(int[][] grid, int row, int col) {
+        grid[row][col]=1;
+        int m = grid.length;
+        int n = grid[0].length;
+        int count=0;
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j] == 1 && !visited[i][j]){
+                    count++;
+                    bfs(grid, visited, i, j);
+                }
             }
-        }
-        if(count == 0){
-            Integer[][] map = newMap(maps.get(0).length, maps.get(0)[0].length);
-            map[position[0]][position[1]] = 1;
-            maps.add(map);
-            count++;
         }
         return count;
     }
 
-    private int[][] directions = new int[][]{{1,0}, {-1,0}, {0,0}, {0,1}, {0,-1}};
-    private boolean adjacentLandExist(Integer[][] map, int[] position) {
-        int i = map.length, j = map[0].length;
-        for(int[] direction: directions){
-            int m = position[0] + direction[0];
-            int n = position[1] + direction[1];
-            if(m>=0 && m<i && n>=0 && n<j && map[m][n]==1)  return true;
+    private void bfs(int[][] grid, boolean[][] visited, int i, int j) {
+        Queue<Integer[]> queue = new LinkedList<>();
+        queue.add(new Integer[]{i,j});
+        while(!queue.isEmpty()){
+            Integer[] point = queue.poll();
+            visited[point[0]][point[1]] = true;
+            for(int[] direction: directions){
+                int newRow = point[0]+direction[0];
+                int newCol = point[1]+direction[1];
+                if(newRow>=0&&newRow<grid.length && newCol>=0&&newCol<grid[0].length){
+                    if(grid[newRow][newCol]==1 && !visited[newRow][newCol]){
+                        queue.add(new Integer[]{newRow, newCol});
+                    }
+                }
+            }
         }
-        return false;
     }
 }
