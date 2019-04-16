40c5ee685adffd51a283c478bae4c2cc438d119a
karlbishnu <karlbishnu@gmail.com>
2019-04-16 21:21:09 +0900

    AlgoSpot - MERCY https://algospot.com/judge/problem/read/MERCY

---
+++ algospot/src/main/java/mercy/Main.java
@@ -0,0 +1,21 @@
+package mercy;
+/**
+ * https://algospot.com/judge/problem/read/MERCY
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner in;
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        while(cases-- > 0) {
+            main2();
+        }
+    }
+
+    private static void main2() {
+        System.out.println("Hello Algospot!");
+    }
+}
